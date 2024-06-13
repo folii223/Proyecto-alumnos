bfdf58ca3d3a8e144ecee99bdbe022d10ee1c265
folii223 <117038834+folii223@users.noreply.github.com>
2024-06-13 19:07:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Transactions;
+
+namespace ProyectoAlumnos
+{
+    internal class Program
+    {
+        static string leerOpcionAlumno()
+        {
+            Console.WriteLine("*------------------------------------*");
+            Console.WriteLine("*    Ingrese una opcion válida       *");
+            Console.WriteLine("*      1- Alta de usuario:           *");
+            Console.WriteLine("*      2- Baja de usuario:           *");
+            Console.WriteLine("*      3- Modificación de usuario:   *");
+            Console.WriteLine("*      0- Salir del menú             *");
+            Console.WriteLine("*------------------------------------*");
+            Console.Write("Opción: ");
+
+            string opcion;
+            do
+            {
+                leerOpcionAlumno();
+                opcion = Console.ReadLine();
+                if (opcion == "1")
+                {
+                    //alta usuario
+                }
+                else if (opcion == "2")
+                {
+                    //baja usuario
+                }
+                else if (opcion == "3")
+                {
+                    //modificacion usuario
+                }
+
+
+            } while (opcion != "0");
+
+                
+            
+        }
+        static void Main(string[] args)
+        {         
+            string opcionMenuAlumno;
+            
+            
+            do
+            {
+
+
+                opcionMenuAlumno = leerOpcionAlumno();
+            
+            
+            } while (true);
+        }
+    }
+}
